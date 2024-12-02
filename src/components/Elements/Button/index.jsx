875c3c1c66b89df7891b5dd3eeb3f7ac99a868da
maqilfahmi <111202214001@mhs.dinus.ac.id>
2024-12-02 17:50:15 +0700

    2 des

--- src/components/Elements/Button/index.jsx
+++ src/components/Elements/Button/index.jsx
@@ -1,20 +1,11 @@
-const Input = (props) => {
-  const {
-    type,
-    placeholder,
-    name,
-    variant = "bg-special-mainBg border border-gray-03",
-  } = props;
+const Button = (props) => {
+  const { children, variant, type } = props;
 
   return (
-    <input
-      type={type}
-      className={`${variant} py-3 ps-4 text-sm  rounded-md w-full text-gray-01 focus:border-black focus:outline-none focus:ring-0`}
-      placeholder={placeholder}
-      name={name}
-      id={name}
-    />
+    <button className={`h-12 rounded-md text-sm ${variant}`} type={type}>
+      {children}
+    </button>
   );
 };
 
-export default Input;
+export default Button;
