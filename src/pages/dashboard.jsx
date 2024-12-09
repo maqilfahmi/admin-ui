1e7aa3899297e3745743e0100cdc02c641007430
maqilfahmi <111202214001@mhs.dinus.ac.id>
2024-12-10 00:29:31 +0700

    9des

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,5 +1,6 @@
+import Card from "../components/Elements/Card";
 import MainLayout from "../components/Layouts/MainLayout";
-import CardBill from "../components/Fragments/Dashboard/CardBill";
+import CardBill from "../components/Fragments/dashboard/CardBill";
 import CardExpenseBreakdown from "../components/Fragments/Dashboard/CardExpenseBreakdown.jsx";
 import CardTransaction from "../components/Fragments/Dashboard/CardTransaction.jsx";
 import CardBalance from "../components/Fragments/Dashboard/CardBalance";
